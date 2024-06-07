ddc5a7efbea629bfcbec82554181b5ee28383a56
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-06 19:41:32 -0500

    chore: Add categories and products seed data

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+import { categories } from './data/categories'
+import { products } from './data/products'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  try {
+    await prisma.category.createMany({
+      data: categories,
+    })
+    await prisma.product.createMany({
+      data: products,
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
