fa6dcc6ae783de522261cbe04217e9173f72aea3
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-07 03:41:51 -0500

    chore: Delete `price` column from `OrderProducts` table

---
+++ prisma/migrations/20240607045129_delete_price_table_from_order_products/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `price` on the `OrderProducts` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "OrderProducts" DROP COLUMN "price";
