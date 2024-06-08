8bc3808b967ba2b53d9e6e4871a4737774c2f457
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-08 00:24:52 -0500

    refactor: Update OrderWithProducts type definition

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,6 +1,10 @@
-import { Product } from '@prisma/client'
+import { Order, OrderProducts, Product } from '@prisma/client'
 
 export type OrderItem = Pick<Product, 'id' | 'name' | 'price'> & {
   quantity: number
   subtotal: number
+}
+
+export type OrderWithProducts = Order & {
+  orderProducts: (OrderProducts & { product: Product })[]
 }
