6783034097e4f0a34879168743dd4434adfbcf11
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-08 00:25:06 -0500

    refactor: Update completeOrder action to use OrderIdSchema for parsing and validating the orderId value

---
+++ src/actions/complete-order-action.ts
@@ -0,0 +1,29 @@
+'use server'
+
+import { revalidatePath } from 'next/cache'
+import { prisma } from '@/lib/prisma'
+import { OrderIdSchema } from '@/schema'
+
+export async function completeOrder(formData: FormData) {
+  try {
+    const data = {
+      orderId: formData.get('orderId'),
+    }
+    const result = OrderIdSchema.safeParse(data)
+    if (!result.success) throw new Error(result.error.errors[0].message)
+
+    const order = await prisma.order.update({
+      where: {
+        id: result.data.orderId,
+      },
+      data: {
+        status: true,
+      },
+    })
+
+    revalidatePath('/admin/orders')
+    return order
+  } catch (error) {
+    console.log(error)
+  }
+}
