dc39fb21cc2ddf9373a3297016d9543ea4dd9b13
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-08 00:26:24 -0500

    refactor: Remove unused page components

--- src/app/products/new/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function NewProductPage() {
-  return (
-    <div>
-      <h1>Hello Page</h1>
-    </div>
-  )
-}
--- src/app/products/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function ProductsPage() {
-  return (
-    <div>
-      <h1>Products Page</h1>
-    </div>
-  )
-}
