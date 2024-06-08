0edeb2e971f2d353f8672c3ad9e6d5898141f0e7
iAngelManuel <iangelmanueldm@gmail.com>
2024-06-08 02:58:07 -0500

    feat: Add ProductSchema for validating product input

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -26,3 +26,23 @@ export const OrderIdSchema = z.object({
 export const SearchSchema = z.object({
   search: z.string().trim().min(1, 'La búsqueda no puede estar vacía'),
 })
+
+export const ProductSchema = z.object({
+  name: z
+    .string()
+    .trim()
+    .min(1, { message: 'El Nombre del Producto no puede ir vacio' }),
+  price: z
+    .string()
+    .trim()
+    .transform((value) => parseFloat(value))
+    .refine((value) => value > 0, { message: 'Precio no válido' })
+    .or(z.number().min(1, { message: 'La Categoría es Obligatoria' })),
+  categoryId: z
+    .string()
+    .trim()
+    .transform((value) => parseInt(value))
+    .refine((value) => value > 0, { message: 'La Categoría es Obligatoria' })
+    .or(z.number().min(1, { message: 'La Categoría es Obligatoria' })),
+  image: z.string().min(1, { message: 'La imagen es obligatoria' }),
+})
